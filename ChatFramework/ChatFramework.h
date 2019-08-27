cce18a4322fcf01e2e19d2b1144e7c5cd149295a
Shehzad <Shehzad@fomarkmedia.com>
2019-08-27 22:13:08 +0500

    Initial

---
+++ ChatFramework/ChatFramework.h
@@ -0,0 +1,19 @@
+//
+//  ChatFramework.h
+//  ChatFramework
+//
+//  Created by Apple on 27/08/2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChatFramework.
+FOUNDATION_EXPORT double ChatFrameworkVersionNumber;
+
+//! Project version string for ChatFramework.
+FOUNDATION_EXPORT const unsigned char ChatFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatFramework/PublicHeader.h>
+
+
